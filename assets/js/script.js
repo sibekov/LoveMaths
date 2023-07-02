cff2b1a6681c4a28cde2f54c41688ba0f4c35ec5
sibekov <>
2023-07-02 17:48:41 +0000

    Add multiplication code to the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,6 +29,9 @@ function runGame(gameType) {
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
     }
+    else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
+    }
     else {
         alert(`Unknown game type:${gameType}`);
         throw `Unknown game type:${gameType}.Aborting!`;
@@ -67,6 +70,8 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
     }
     else {
         alert(`Unimplemented operator ${operator}`);
@@ -100,8 +105,10 @@ function displaySubtractionQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
-
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 }
 
 function displayDivisionQuestion() {
